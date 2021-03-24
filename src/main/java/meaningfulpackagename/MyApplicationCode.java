fd68f214adf274a7fc23a5c87bb9934c5765b1ec
yiji-zhang-rhit <yiji.zhang@rose-huilman.edu>
2021-03-24 13:40:02 -0400

    added the project files: build script and code

---
+++ src/main/java/meaningfulpackagename/MyApplicationCode.java
@@ -0,0 +1,8 @@
+package meaningfulpackagename;
+
+public class MyApplicationCode {
+
+  void myMethod1() {
+    
+  }
+}
---
+++ src/test/java/meaningpackagename/MyApplicationCodeTests.java
@@ -0,0 +1,13 @@
+package meaningpackagename;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class MyApplicationCodeTests {
+
+  @Test
+  void testMyMethod1Case1() {
+    fail("Not yet implemented");
+  }
+}
